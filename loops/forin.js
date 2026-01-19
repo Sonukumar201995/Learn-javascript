39a48f6c480fa7040450e52555f55e494df02527
Sonu Kumar <krsonu025@gmail.com>
2026-01-19 14:07:12 +0530

    add for loops and types

---
+++ loops/forin.js
@@ -0,0 +1,16 @@
+let person = {
+    name: "Sonu",
+    age: 25,
+    role: "Developer"
+};
+
+for (let key in person) {
+    console.log(key, ":", person[key]);
+}
+
+
+let a=[10,20,30,40,50]
+for(let b of a)
+{
+    console.log(b);
+}
---
+++ loops/forloop.js
@@ -0,0 +1,27 @@
+
+// for loops
+let a=10;
+
+for(let i=1; i<=10; i++)
+{
+    console.log(i);
+}
+
+// while loops
+
+let num=1;
+while(num<=10)
+{
+    console.log("while loops "+num);
+    num++;
+}
+
+
+// dowhileloop
+
+let a1=1;
+do{
+    console.log(a1);
+    a1++;
+}
+while(a1<=10);
