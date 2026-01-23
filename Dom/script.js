72a56b06e836963060987f32763c659ca3d3d9dd
Sonu Kumar <krsonu025@gmail.com>
2026-01-23 17:25:46 +0530

    dom walking

---
+++ Dom/script.js
@@ -0,0 +1,7 @@
+let a = document.head;
+let b = document.body;
+let c = document.getElementsByTagName("p");
+
+console.log(a); // head
+console.log(b); // body
+console.log(c); // HTMLCollection of p
