549f139bd08df5058dc64e7c71a651c0d5d0011a
Sonu Kumar <krsonu025@gmail.com>
2026-01-27 15:47:23 +0530

    settimeout and set interval

---
+++ timeinterval/setimeout.js
@@ -0,0 +1,17 @@
+let sum = () => {
+    setTimeout(() => {
+        console.log("hello")
+    }, 2000);
+}
+
+sum();
+
+
+// settimeinterval
+
+let msg=()=>{
+    setInterval(()=>{
+        console.log('hello wrold')
+    },2000)
+}
+msg();
