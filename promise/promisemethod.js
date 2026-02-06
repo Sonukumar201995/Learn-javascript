4e51867aa7ff66a513b4614e9b42d180a764dcae
Sonu Kumar <krsonu025@gmail.com>
2026-02-06 17:26:14 +0530

    promisemethods

--- promise/promisemethod.js
+++ promise/promisemethod.js
@@ -1,19 +1,19 @@
 let p1=new Promise((resolve,reject)=>{
     setTimeout(()=>{
         resolve('value 1')
-    })
+    },1000)
 })
 
 let p2=new Promise((resolve,reject)=>{
     setTimeout(()=>{
         resolve('value 2')
-    })
+    },2000)
 })
 
 let p3=new Promise((resolve,reject)=>{
     setTimeout(()=>{
         resolve('value 3')
-    })
+    },3000)
 })
 
 Promise.all([p1, p2, p3])
